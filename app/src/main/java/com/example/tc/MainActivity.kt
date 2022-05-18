aae25c43d8f8f1345b722ad5bfbd983ed8886766
Sujeet-Kumar0 <sujetkumar06@gmail.com>
2022-05-19 00:01:31 +0530

    Minor Changes

--- app/src/main/java/com/example/tc/MainActivity.kt
+++ app/src/main/java/com/example/tc/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.tc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import android.text.Editable
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    /*
+    startbutton.setOnClickListener{
+        navigateUpTo()
+    }
+    val view = inflater.inflate(R.layout.activity_main, container, false)
+    view.startbutton.setOnClickListener {
+    */
 }
