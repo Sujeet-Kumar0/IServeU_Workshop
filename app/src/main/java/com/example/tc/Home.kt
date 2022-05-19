1fff51db39ccd6a5f096328bee8793c81e9d79e4
badal <badal.bahinipati99@gmail.com>
2022-05-19 22:36:15 +0530

    Homepage commit

--- app/src/main/java/com/example/tc/Home.kt
+++ app/src/main/java/com/example/tc/Home.kt
@@ -2,30 +2,10 @@ package com.example.tc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 
-class HomePage : AppCompatActivity() {
+class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                //.add(R.id.container, activity_login())
-                .commit()
-        }
-    }
-
-    fun navigateTo(fragment: Fragment, addToBackstack: Boolean) {
-        val transaction = supportFragmentManager
-            .beginTransaction()
-        .replace(R.id.container, fragment)
-
-        if (addToBackstack) {
-            transaction.addToBackStack(null)
-        }
-
-        transaction.commit()
     }
 }
-
