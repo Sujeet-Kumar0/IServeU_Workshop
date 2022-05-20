455e321d3469fad61decd6e3b7012e5ad9776fba
Sujeet-Kumar0 <sujetkumar06@gmail.com>
2022-05-20 13:55:46 +0530

    Login Functional

--- app/src/main/java/com/example/tc/Home.kt
+++ app/src/main/java/com/example/tc/Home.kt
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class Home : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.frame_container, HomeFragment())
+                .commit()
+        }
 
 
     }
